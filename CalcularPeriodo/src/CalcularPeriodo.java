ca37cae99dbd6290edfd2845a8bc07c4a302fe21
Yumurdzhan <yumurdzhan17@gmail.com>
2019-05-20 20:31:16 +0200

    Modificando Main

--- CalcularPeriodo/src/CalcularPeriodo.java
+++ CalcularPeriodo/src/CalcularPeriodo.java
@@ -2,30 +2,38 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 
-public class CalcularPeriodo {
-	
-	public static void main(String[] args) {
-		
-		//Creamos la fecha del descubrimiento de américa
-		LocalDate fecha = LocalDate.of(1492, Month.OCTOBER, 12);	 
+public class CalcularPeriodo
+{
+
+	public static void main(String[] args)
+	{
+
+		// Creamos la fecha del descubrimiento de américa
+		LocalDate fecha = LocalDate.of(1492, Month.OCTOBER, 12);
 		// Mostramos cuánto tiempo ha pasado
 		calcularPeriodo("El descubrimiento de América", fecha);
-		
+
+		// Creamos la fecha de la conquista de Constantinopla
+		LocalDate fecha2 = LocalDate.of(1453, Month.MAY, 29);
+		// Mostramos cuánto tiempo ha pasado
+		calcularPeriodo("La caida de Constantinopla", fecha2);
+
 	}
-	
-	public static void calcularPeriodo(String nombre, LocalDate fecha) {
-        
-        LocalDate fechaActual = LocalDate.now();
-        Period periodo = Period.between(fecha, fechaActual);
-        
-        // Cálculo de las diferencias
-        int anyos = periodo.getYears();
-        int meses = periodo.getMonths();
-        int dias = periodo.getDays();
-        
-        String texto = String.format(nombre + " ocurrió hace %d años, %d meses y %d días.", anyos, meses, dias);
-         
-        System.out.println(texto);
-    }
- 
+
+	public static void calcularPeriodo(String nombre, LocalDate fecha)
+	{
+
+		LocalDate fechaActual = LocalDate.now();
+		Period periodo = Period.between(fecha, fechaActual);
+
+		// Cálculo de las diferencias
+		int anyos = periodo.getYears();
+		int meses = periodo.getMonths();
+		int dias = periodo.getDays();
+
+		String texto = String.format(nombre + " ocurrió hace %d años, %d meses y %d días.", anyos, meses, dias);
+
+		System.out.println(texto);
+	}
+
 }
